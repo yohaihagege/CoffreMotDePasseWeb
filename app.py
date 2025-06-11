60f698b53e2c1e1c2c24df4c2a0466e55add5234
Yohai <hagegeyohai@gmail.com>
2025-06-11 16:08:25 +0200

    creation de la page d'accueil

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+import os, json, base64
+from cryptography.fernet import Fernet, InvalidToken
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 
 app = Flask(__name__)
+app.secret_key = 'vraiment_tres_secret'
 
-@app.route('/')
-def index():
-    return render_template('index.html')  # À créer dans /templates
+sel = b'mon_sel_unique_et_constant'
 
-if __name__ == '__main__':
+def generer_cle(mdp):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=sel,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return base64.urlsafe_b64encode(kdf.derive(mdp.encode()))
+
+@app.route('/', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        mdp = request.form['mdp_maitre']
+        try:
+            cle = generer_cle(mdp)
+            session['cle'] = cle.decode()
+            return redirect('/dashboard')
+        except:
+            return "Erreur dans la génération de la clé."
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    if 'cle' not in session:
+        return redirect('/')
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
