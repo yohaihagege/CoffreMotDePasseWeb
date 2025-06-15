69d7f1ab28f0db1e418cd9ad87afde57fca4dc37
Yohai <hagegeyohai@gmail.com>
2025-06-15 18:29:35 +0200

    ajout de la page secondary.html et dashboard.html ainsi que leur code

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+
 app = Flask(__name__)
 app.secret_key = "ta_clef_secrete"  # À personnaliser
 csrf = CSRFProtect(app)
+
+
 from app import routes # ⬅️ important : doit être après la création de l'app
 
 app.register_blueprint(routes.routes)  # ⬅️ enregistre les routes du blueprint
